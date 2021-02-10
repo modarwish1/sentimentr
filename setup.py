6a1a79542bf5e695f384483f57b7a1268a083439
Mohammad Darwich <modarwish1@users.noreply.github.com>
2021-02-10 14:20:55 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+  name = 'sentimentr',       
+  packages = ['sentimentr'],   
+  version = '1',
+  package_data = {'sentimentr': ['lexica/*.txt']},  
+  license='MIT',        
+  description = 'Context-aware sentiment analysis model for formal and informal social media-style parlance',   
+  author = 'Mohammad Darwich',                   
+  author_email = 'modarwish@hotmail.com',     
+  url = 'https://github.com/modarwish1/sentimentr',   
+  download_url = '',    
+  keywords = ['sentiment analysis', 'opinion mining', 'sentiment strength', 'sentiment lexicon', 'sentiment dictionary', 'informal text', 'internet slang', 'social media'],   
+  install_requires=[           
+          'nltk',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',    
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',    
+  ],
+)
